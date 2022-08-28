479b0f83a395b019265f16afab20ef8e701c3817
[George Seletski] <[george.seletski.1224@gmail.com]>
2022-08-28 12:31:34 +0300

    feat: added dark-light switch

---
+++ js/dark_mode.js
@@ -0,0 +1,9 @@
+function toLight() {
+  document.body.classList.remove('dark');
+  document.body.classList.add('light');
+}
+
+function toDark() {
+  document.body.classList.remove('light');
+  document.body.classList.add('dark');
+}
